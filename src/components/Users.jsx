d936af6589f54ccaab5ce9220b8e5618ff538263
Emon <emon.mhk69@gmail.com>
2023-05-07 01:17:02 +0600

    delete done a single user

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,13 +3,25 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
 
     const users = useLoaderData();
+    
+    const handleDelete = (id)=>{
+        fetch(`http://localhost:5000/users/${id}`,{
+            method: "DELETE",
+
+        })
+        .then(res=> res.json())
+        .then(data=> console.log(data))
+        .catch(er=> console.log(er.message))
+    }
 
     return (
         <div>
             <h3>Totall users: {users.length}</h3>
             <div className="">
                 {
-                    users.map(user=> <p key={user._id}> {user.name} : {user.email} </p>)
+                    users.map(user=> <p key={user._id}>
+                     {user.name} : {user.email} <button onClick={()=> handleDelete(user._id)}> X </button> 
+                     </p>)
                 }
             </div>
         </div>
